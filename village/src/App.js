0393ee222db5c0b39a8d433388438cac891fb3ba
Lohrem <30306978+Lohrem@users.noreply.github.com>
2019-06-15 13:49:30 -0400

    Routes are working on App.js

--- village/src/App.js
+++ village/src/App.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
-import { Route } from 'react-router-dom'
+import { Route, NavLink } from 'react-router-dom'
 
 class App extends Component {
   constructor(props) {
@@ -43,9 +43,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <SmurfForm addSmurf={this.addSmurf} />
+        <nav>
+          <NavLink to="/">Home</NavLink> <br></br>
+          <NavLink to="/smurf-form">Add a Smurf</NavLink>
+        </nav>
+        {/* <SmurfForm addSmurf={this.addSmurf} /> */}
         <Route exact path="/" render={(props) => <Smurfs {...props} smurfs={this.state.smurfs}/>} />
-        <Route path="/smurf-form" component={SmurfForm} />
+        <Route path="/smurf-form" render={props => <SmurfForm {...props} addSmurf={this.addSmurf} />}/>
       </div>
     );
   }
